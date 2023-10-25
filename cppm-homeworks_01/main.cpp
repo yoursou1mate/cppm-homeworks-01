6f08f1394b174b306b52aa5446b9ffbce9732147
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-10-25 17:50:46 +0300

    01_Fixed

--- cppm-homeworks_01/main.cpp
+++ cppm-homeworks_01/main.cpp
@@ -11,24 +11,21 @@
 
 
 
-int n;
-int m;
 
-int *arr1 = new int[n]{};;
-int *arr2 = new int[m]{};
 
 
 
 
 int main(int argc, const char * argv[]) {
 
+    // открытие и считывание файла
     std::string line;
     
-    std::ifstream fin("File.txt");
-    fin >> line;
-    
-    
-    std::cout << fin.is_open() << std::endl;
+    std::ifstream fin("in.txt");
+
+   
+
+//    std::cout << fin.is_open() << std::endl;
         if (fin.is_open())
         {
             while (std::getline(fin, line))
@@ -38,22 +35,42 @@ int main(int argc, const char * argv[]) {
         } else
         {
             std::cout << "Error while opening file" << std::endl;
+            return (0);
         }
-
-   
-    std::cout  << n << std::endl;
-    for (int i = 0; i<n; ++i){
-        std::cout << arr1[i] << " ";
+ 
+    fin.close();
+    
+    fin.open("in.txt");
+    
+    //Запись чисел в переменные
+    int n;
+    int m;
+    
+    fin >> n;
+    int *arr1 = new int[n]{};
+    for (int i = 0; i<n; ++i)
+    {
+        fin >> arr1[i];
     }
-    std::cout << std::endl;
-
-
-    std::cout  << m << std::endl;
-    for (int j = 0; j<m; ++j){
-        std::cout << arr2[j] << " ";
+    fin >> m;
+    int *arr2 = new int[m]{};
+    for (int j = 0; j<m; ++j)
+    {
+        fin >> arr2[j];
     }
-
-    std::cout << '\n';
+    
+//    std::cout << n << std::endl;
+//    for (int i = 0; i<n; ++i){
+//        std::cout << arr1[i] << " ";
+//    }
+//    std::cout << std::endl;
+//
+//
+//    std::cout  << m << std::endl;
+//    for (int j = 0; j<m; ++j){
+//        std::cout << arr2[j] << " ";
+//    }
+//    std::cout << '\n';
 
     std::ofstream fout2("out.txt");
     fout2 << m << std::endl;
@@ -70,6 +87,8 @@ int main(int argc, const char * argv[]) {
     }
     fout2 << temp2;
 
+    
+    // Вывод результата
     std::cout << m << '\n';
     std::cout << temp1 << " ";
     for (int j = 0; j<m-1; ++j){
